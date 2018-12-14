100359f1f58ffa15fbc6089f3f32d05dd0ec21bd
Mihai Codoban <micodoba@microsoft.com>
2018-12-14 10:54:11 -0800

    6old

---
+++ synthetic/sdk/working/6Old/build.cmd
@@ -0,0 +1,7 @@
+setlocal
+
+set msbuild="E:\projects\msbuild\artifacts\Debug\bootstrap\net472\MSBuild\Current\Bin"
+
+echo %msbuild%
+
+E:\projects\MSBuildTestProjects\src\msb\bin\Debug\net472\msb.exe %msbuild% "E:\projects\MSBuildTestProjects\synthetic\sdk\working\6Old\caches" "E:\projects\MSBuildTestProjects\synthetic\sdk\working\6Old"
