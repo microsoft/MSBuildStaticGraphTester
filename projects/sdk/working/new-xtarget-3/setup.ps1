ee07e3ffa2950dd54f6356fcf5bfdddf24c8212e
Mihai Codoban <micodoba@microsoft.com>
2019-01-22 10:17:06 -0800

    Add smaller repro

--- projects/sdk/working/new-xtarget-3/setup.ps1
+++ projects/sdk/working/new-xtarget-3/setup.ps1
@@ -3,11 +3,11 @@
 taskkill /f /im StructuredLogViewer.exe
 
 Remove-Item -Force -Recurse **\bin
-dotnet restore $PSScriptRoot\new-xtarget-5.sln
+dotnet restore $PSScriptRoot\1\1.csproj
 msbuild 1\1.csproj /bl:working.binlog
 
 Remove-Item -Force -Recurse **\bin
-dotnet restore $PSScriptRoot\new-xtarget-5.sln
+dotnet restore $PSScriptRoot\1\1.csproj
 msbuild 1\1.csproj /bl:failing.binlog /p:DisableTransitiveProjectReferences=true
 
 start working.binlog
