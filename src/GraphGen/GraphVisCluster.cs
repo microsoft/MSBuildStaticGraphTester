f7d3e58b39dd42cd0f3aa1a64feaff765b86ca58
Andy Gerlicher <angerlic@microsoft.com>
2019-04-17 16:16:56 -0700

    Add GraphGen.exe

---
+++ src/GraphGen/GraphVisCluster.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphGen
+{
+    public class GraphVisCluster
+    {
+        private static int _clusterIdGlobal;
+        private static int _clusterId;
+
+        private readonly List<GraphVisNode> _nodes = new List<GraphVisNode>();
+        private readonly string _clusterLabel;
+
+        public GraphVisCluster(string projectPath)
+        {
+            _clusterLabel = Path.GetFileName(projectPath);
+            _clusterId = _clusterIdGlobal;
+            _clusterIdGlobal++;
+        }
+
+        public void AddNode(GraphVisNode node)
+        {
+            _nodes.Add(node);
+        }
+
+        public string Create()
+        {
+            if (_nodes.Count == 1)
+            {
+                return _nodes[0].Create();
+            }
+
+            var result = $@"
+        subgraph cluster_{_clusterId} {{
+		style=filled;
+		color=lightgrey;
+		node [style=filled,color=white];
+		label = ""{_clusterLabel}"";";
+
+            foreach (var p in _nodes)
+            {
+                result += p.Create();
+            }
+
+            result += "}";
+            return result;
+
+        }
+
+    }
+}
---
+++ src/GraphGen/GraphVisEdge.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Build.Experimental.Graph;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphGen
+{
+    public class GraphVisEdge
+    {
+        private readonly GraphVisNode _node1;
+        private readonly GraphVisNode _node2;
+
+        public GraphVisEdge(GraphVisNode node1, GraphVisNode node2)
+        {
+            _node1 = node1;
+            _node2 = node2;
+        }
+
+        public string Create()
+        {
+            return $"  {_node1.Name} -> {_node2.Name};"; //[color=\"0.002 0.999 0.999\"];";
+        }
+    }
+}
