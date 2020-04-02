2e18f83e449f14db582a48ec66514343a745cab1
Mihai Codoban <micodoba@microsoft.com>
2020-04-02 14:21:35 -0700

    add sample with transitive project references

---
+++ projects/sdk/working/transitiveReferences/A/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using B;
+using C;
+
+namespace A
+{
+    public class Class1
+    {
+        public static int A = B.Class1.B + C.Class1.C;
+    }
+}
---
+++ projects/sdk/working/transitiveReferences/C/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace C
+{
+    public class Class1
+    {
+        public static int C;
+    }
+}
